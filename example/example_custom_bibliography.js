eba1fcdd938944b99e3a2df3c99135bcf9f4d234
michaelplews <michael.plews@gmail.com>
2016-09-10 09:26:19 -0500

    Tidying

---
+++ example/example_custom_bibliography.js
@@ -0,0 +1,15 @@
+var bibliography = "@article{Zhang2012,\
+abstract = {We report ultrasensitive sub-10 nm NaMnF(3) nanocrystals codoped with Yb(3+), Er(3+)/Tm(3+) ions, and their intense pure red and near-infrared upconversion emissions in the presence of Mn(2+). The nanocrystals showed excellent T(1) contrast in 7 T MRI, implying their potential as single-phase contrast agents for fluorescent deep tissue and MR imaging.},\
+author = {Zhang, Yan and Lin, Jing Dong and Vijayaragavan, Vimalan and Bhakoo, Kishore K and Tan, Timothy Thatt Yang},\
+doi = {10.1039/c2cc34858f},\
+issn = {1364-548X},\
+journal = {Chem. Commun. (Camb).},\
+month = {oct},\
+number = {83},\
+pages = {10322--4},\
+pmid = {22936349},\
+title = {{Tuning sub-10 nm single-phase NaMnF3 nanocrystals as ultrasensitive hosts for pure intense fluorescence and excellent T1 magnetic resonance imaging}},\
+url = {http://www.ncbi.nlm.nih.gov/pubmed/22936349},\
+volume = {48},\
+year = {2012}\
+}"
